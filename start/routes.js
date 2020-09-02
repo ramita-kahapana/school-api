1b9cd085487fe69f47353a9b40118923d80d21b9
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-02 23:53:53 +0700

    edit-routes

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const SubjectController = require('../app/Controllers/Http/SubjectController')
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -21,24 +23,36 @@ Route.get('/', () => {
 })
 
 Route.group(() => {
-    Route.get('/teachers', 'TeacherController.index')
-    Route.get('/teachers/:id', 'TeacherController.show')
-    Route.post('/teachers', 'TeacherController.store')
+    Route.resource('/teachers', 'TeacherController')
+        //Route.get('/teachers', 'TeacherController.index')
+        //Route.get('/teachers/:id', 'TeacherController.show')
+        //Route.post('/teachers', 'TeacherController.store')
+        //Route.put('/teachers/:id', 'TeacherController.update')
+        //Route.patch('/teachers/:id', 'TeacherController.update')
+        //Route.delete('/teachers/:id', 'TeacherController.destroy')
 
-    Route.get('/students', 'StudentController.index')
-    Route.get('/students/:id', 'StudentController.show')
-    Route.post('/students', 'StudentController.store')
+    Route.resource('/students', 'StudentController')
+    Route.get('students/:id/group', 'StudentController.showGroup')
+        //Route.get('/students', 'StudentController.index')
+        //Route.get('/students/:id', 'StudentController.show')
+        //Route.post('/students', 'StudentController.store')
 
-    Route.get('/subjects', 'SubjectController.index')
-    Route.get('/subjects/:id', 'SubjectController.show')
-    Route.post('/subjects', 'SubjectController.store')
+    Route.resource('/subjects', 'SubjectController')
+    Route.get('subjects/:id/teacher', 'SubjectController.showTeacher') //อยู่ในparamsหมด
+        //Route.get('/subjects', 'SubjectController.index')
+        //Route.get('/subjects/:id', 'SubjectController.show')
+        //Route.post('/subjects', 'SubjectController.store')
 
-    Route.get('/groups', 'GroupController.index')
-    Route.get('/groups/:id', 'GroupController.show')
-    Route.post('/groups', 'GroupController.store')
+    Route.resource('/enrollments', 'EnrollmentController')
 
-    Route.get('/enrollments', 'EnrollmentController.index')
-    Route.get('/enrollments/:id', 'EnrollmentController.show')
-    Route.post('/enrollments', 'EnrollmentController.store')
+    //Route.get('/groups', 'GroupController.index')
+    //Route.get('/groups/:id', 'GroupController.show')
+    //Route.post('/groups', 'GroupController.store')
+
+    Route.resource('/groups', 'GroupController')
+        //Route.get('/enrollments', 'EnrollmentController.index')
+        //Route.get('/enrollments/:id', 'EnrollmentController.show')
+        //Route.post('/enrollments', 'EnrollmentController.store')
+
 
 }).prefix('api/v1')
