4356c59c4a74f71f39ec137d6a2c8d1f82f48dd9
ramita-kahapana <ramitakahapana@gmail.com>
2020-09-02 23:50:55 +0700

    edit-group

--- app/Controllers/Http/GroupController.js
+++ app/Controllers/Http/GroupController.js
@@ -1,5 +1,7 @@
 'use strict'
 const Database = use('Database')
+const Validator = use('Validator')
+const Group = use("App/Models/Group")
 
 function numberTypeParamValidator(number) {
     if (Number.isNaN(parseInt(number)))
@@ -10,10 +12,14 @@ function numberTypeParamValidator(number) {
 }
 
 class GroupController {
-    async index() {
-        const groups = await Database.table('groups')
-
-        return { status: 200, error: undefined, data: groups }
+    async index({ request }) {
+        const { references = undefined } = request.qs
+        const groups = Group.query()
+        if (references) {
+            const extractedReferences = references.split(",")
+            groups.with(extractedReferences)
+        }
+        return { status: 200, error: undefined, data: await groups.fetch() }
     }
     async show({ request }) {
         const { id } = request.params
@@ -36,20 +42,61 @@ class GroupController {
 
     async store({ request }) {
         const { name } = request.body
-        const missingKeys = []
-        if (!name) missingKeys.push('name')
+            //const missingKeys = []
+        const rules = {
+            name: 'required'
+
+        }
+        const validation = await Validator.validateAll(request.body, rules)
+
+        if (validation.fails()) //true and fail
+            return { status: 422, error: validation.messages(), data: undefined }
+            //if (!name) missingKeys.push('name')
+
+        const group = new Group();
+        group.name = name;
+
+        await group.save()
+
+        return { status: 200, error: undefined, data: group }
+
+        //if (missingKeys.length)
+        //return { status: 422, error: `${missingKeys} is missing.`, data: undefined } //ของมาไม่ครบก็คือ422
 
 
 
-        if (missingKeys.length)
-            return { status: 422, error: `${missingKeys} is missing.`, data: undefined } //ของมาไม่ครบก็คือ422
+        //.table('groups')
+        //.insert({ name }) //add to database
+        //return { status: 200, error: undefined, data: { name } }
+    }
+    async update({ request }) {
+        //const body =request.body
+        //const params =request.params
+        const { body, params } = request
+        const { id } = params
+        const { name } = body
 
-
+        const groupId = await Database
+            .table('groups')
+            .where({ group_id: id })
+            .update({ name })
 
         const group = await Database
             .table('groups')
-            .insert({ name }) //add to database
-        return { status: 200, error: undefined, data: { name } }
+            .where({ group_id: groupId })
+            .first() //rerunid
+
+        return { status: 200, error: undefined, data: group }
+    }
+    async destroy({ request }) {
+        const { id } = request.params
+
+        await Database
+            .table('groups')
+            .where({ group_id: id })
+            .delete()
+
+        return { status: 200, error: undefined, data: { maessage: 'success' } }
     }
 }
 
